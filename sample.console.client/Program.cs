a1c740c8813bfbddb13e45970f52f60988daa4cf
Cengiz Han <cengiz@cengizhan.com>
2013-03-16 01:06:39 +0000

    added an console application which continusly logs an error and info message every minute but log level is ERROR so INFO does not get published to rabbitmq.

---
+++ sample.console.client/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using log4net;
+using log4net.Config;
+
+namespace sample.console.client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            XmlConfigurator.Configure();
+            using (new ForeverLoggingClass())
+            {
+                Console.WriteLine("Will keep logging a message per second.");
+                Console.ReadLine();
+            }
+        }
+    }
+
+    public class ForeverLoggingClass : IDisposable
+    {
+        protected static readonly ILog Log = LogManager.GetLogger(typeof(ForeverLoggingClass));
+        private readonly CancellationTokenSource cancellationTokenSource;
+
+        public ForeverLoggingClass()
+        {
+            cancellationTokenSource = new CancellationTokenSource();
+            Task.Factory.StartNew(LogSomeStuff, cancellationTokenSource.Token);
+        }
+
+        private void LogSomeStuff()
+        {
+            while (!cancellationTokenSource.Token.IsCancellationRequested)
+            {
+                Log.Info("info message");
+                Log.Error(new Exception("some random exception"));
+                Thread.Sleep(10000);
+            }
+        }
+
+        public void Dispose()
+        {
+            cancellationTokenSource.Cancel();
+        }
+    }
+}
