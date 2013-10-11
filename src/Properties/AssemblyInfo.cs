2e3dd818b21dac6c9b9765133d5fbf7a5ec75894
Cengiz Han <chan@bglgroup.co.uk>
2013-10-11 12:41:50 +0100

    Cengiz : changed nuget_push rake task to read api_key from parameter, not from nuget.apikey file from disk
    usage : rake nuget_push["hidden_api_key_comes_here"]

--- src/Properties/AssemblyInfo.cs
+++ src/Properties/AssemblyInfo.cs
@@ -6,5 +6,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyProduct("rabbitmq.log4net.gelf.appender")]
 [assembly: ComVisible(false)]
 [assembly: Guid("262c1e9b-68ef-4077-9ccf-08fc985a055d")]
-[assembly: AssemblyVersion("0.1.1")]
-[assembly: AssemblyFileVersion("0.1.1")]
+[assembly: AssemblyVersion("0.1.4")]
+[assembly: AssemblyFileVersion("0.1.4")]
