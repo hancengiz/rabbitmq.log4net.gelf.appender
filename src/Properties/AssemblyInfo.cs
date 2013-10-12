d70357aa3c92cea6dc0182a0b0a14ee94d4d8d4b
Marcin Floryan <marcin.floryan@gmail.com>
2013-10-12 20:38:59 +0100

    GUID for the Assembly is only required if it's to be made visible for COM

--- src/Properties/AssemblyInfo.cs
+++ src/Properties/AssemblyInfo.cs
@@ -1,10 +1,7 @@
 ﻿using System.Reflection;
-using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("rabbitmq.log4net.gelf.appender")]
 [assembly: AssemblyCompany("Cengiz Han")]
 [assembly: AssemblyProduct("rabbitmq.log4net.gelf.appender")]
-[assembly: ComVisible(false)]
-[assembly: Guid("262c1e9b-68ef-4077-9ccf-08fc985a055d")]
 [assembly: AssemblyVersion("0.1.4")]
 [assembly: AssemblyFileVersion("0.1.4")]
