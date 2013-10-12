e13e43c3fd3e9de57c4f9b646013073fe7b5f53e
Marcin Floryan <marcin.floryan@gmail.com>
2013-10-12 22:41:57 +0100

    The Facility change probably warrants version upgrade

--- src/Properties/AssemblyInfo.cs
+++ src/Properties/AssemblyInfo.cs
@@ -3,5 +3,5 @@
 [assembly: AssemblyTitle("rabbitmq.log4net.gelf.appender")]
 [assembly: AssemblyCompany("Cengiz Han")]
 [assembly: AssemblyProduct("rabbitmq.log4net.gelf.appender")]
-[assembly: AssemblyVersion("0.1.4")]
-[assembly: AssemblyFileVersion("0.1.4")]
+[assembly: AssemblyVersion("0.1.5")]
+[assembly: AssemblyFileVersion("0.1.5")]
