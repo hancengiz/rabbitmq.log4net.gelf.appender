3b16518eee11338fa457016fa80d6a6c17a764df
Marcin Floryan <marcin.floryan@gmail.com>
2013-10-22 18:35:56 +0100

    The AssemblyInfo includes Desription shared with the NuGet Package description and explicit InformationVersion

--- src/Properties/AssemblyInfo.cs
+++ src/Properties/AssemblyInfo.cs
@@ -5,5 +5,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCompany("Cengiz Han")]
 [assembly: AssemblyProduct("rabbitmq.log4net.gelf.appender")]
 [assembly: AssemblyVersion("0.1.5")]
+[assembly: AssemblyInformationalVersion("0.1.5")]
 [assembly: AssemblyFileVersion("0.1.5")]
+[assembly: AssemblyDescription("Custom log4net appender to publish log4net message to RabbitMQ in GELF format.")]
 [assembly: ComVisible(false)]
