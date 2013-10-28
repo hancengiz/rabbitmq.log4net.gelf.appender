4f912f2bad93556f3bca42bfbfe6d0bb1a8bde09
Marcin Floryan <marcin.floryan@gmail.com>
2013-10-28 21:37:47 +0000

    Better information about the package for the NuGet gallery.

--- src/Properties/AssemblyInfo.cs
+++ src/Properties/AssemblyInfo.cs
@@ -2,10 +2,10 @@
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("rabbitmq.log4net.gelf.appender")]
-[assembly: AssemblyCompany("Cengiz Han")]
 [assembly: AssemblyProduct("rabbitmq.log4net.gelf.appender")]
+[assembly: AssemblyCompany("Cengiz Han, Marcin Floryan")]
 [assembly: AssemblyVersion("0.1.6")]
 [assembly: AssemblyInformationalVersion("0.1.6")]
 [assembly: AssemblyFileVersion("0.1.6")]
-[assembly: AssemblyDescription("Custom log4net appender to publish log4net message to RabbitMQ in GELF format.")]
+[assembly: AssemblyDescription("Custom log4net appender publishing log messages on to RabbitMQ in GELF format. A simple way to integrate your logging with http://logstash.net")]
 [assembly: ComVisible(false)]
