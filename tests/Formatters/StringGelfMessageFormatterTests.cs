fda580efc372cb8be2fc612e689bdbc824575409
Marcin Floryan <marcin.floryan@gmail.com>
2013-11-02 21:48:02 +0000

    Tests for String Formatter
    Cause they not only server as coverage but as documentation

---
+++ tests/Formatters/StringGelfMessageFormatterTests.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using NUnit.Framework;
+using rabbitmq.log4net.gelf.appender;
+using rabbitmq.log4net.gelf.appender.MessageFormatters;
+
+namespace tests.Formatters
+{
+    [TestFixture]
+    public class StringGelfMessageFormatterTests
+    {
+        [Test]
+        public void Can_Be_Applied_To_String_But_Not_Object()
+        {
+            var formatter = new StringGelfMessageFormatter();
+
+            Assert.That(formatter.CanApply("some string"), Is.True);
+            Assert.That(formatter.CanApply(new Object()), Is.False);
+        }
+
+        [Test]
+        public void Formats_Short_String_Into_Both_Short_And_Full_Message()
+        {
+            var formatter = new StringGelfMessageFormatter();
+            
+            var message = GelfMessage.EmptyGelfMessage;
+
+            formatter.Format(message, "Something went wrong");
+
+            Assert.That(message.ShortMessage, Is.EqualTo("Something went wrong"));
+            Assert.That(message.FullMessage, Is.EqualTo("Something went wrong"));
+        }
+
+        [Test]
+        public void Takes_First_250_Characters_From_A_Long_String_As_Short_Message()
+        {
+            var formatter = new StringGelfMessageFormatter();
+
+            var gelfMessage = GelfMessage.EmptyGelfMessage;
+
+            var messageString = "10 letters".Repeat(30);
+            formatter.Format(gelfMessage, messageString);
+
+            Assert.That(gelfMessage.ShortMessage, Is.EqualTo("10 letters".Repeat(25)));
+            Assert.That(gelfMessage.FullMessage, Is.EqualTo(messageString));
+        }
+    }
+}
