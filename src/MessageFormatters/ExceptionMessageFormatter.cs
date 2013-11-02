211f654ad1678ea9f913ee4ae2460d1ec50aeb8e
Marcin Floryan <marcin.floryan@gmail.com>
2013-11-02 23:35:29 +0000

    New Message Formatter for Exception objects
    With the intention that it can be used when we pass Exception as the logged object or as the additional info

---
+++ src/MessageFormatters/ExceptionMessageFormatter.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace rabbitmq.log4net.gelf.appender.MessageFormatters
+{
+    public class ExceptionMessageFormatter : IGelfMessageFormatter
+    {
+        public bool CanApply(object messageObject)
+        {
+            return messageObject is Exception;
+        }
+
+        public void Format(GelfMessage gelfMessage, object messageObject)
+        {
+            var exception = (Exception) messageObject;
+
+            if (string.IsNullOrEmpty(gelfMessage.ShortMessage))
+            {
+                gelfMessage.ShortMessage = exception.Message;
+            }
+            else
+            {
+                gelfMessage["_ExceptionMessage"] = exception.Message;
+            }
+
+            if (string.IsNullOrEmpty(gelfMessage.FullMessage))
+            {
+                gelfMessage.FullMessage = exception.ToString();
+            }
+            else
+            {
+                gelfMessage["_Exception"] = exception.ToString();
+            }
+            
+            gelfMessage["_ExceptionType"] = messageObject.GetType().FullName;
+            gelfMessage["_ExceptionStackTrace"] = exception.StackTrace;
+
+            if (exception.InnerException == null) return;
+
+            gelfMessage["_InnerExceptionType"] = exception.InnerException.GetType().FullName;
+            gelfMessage["_InnerExceptionMessage"] = exception.InnerException.Message;
+        }
+    }
+}
---
+++ tests/Formatters/ExceptionMessageFormatterTests.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using NUnit.Framework;
+using rabbitmq.log4net.gelf.appender;
+using rabbitmq.log4net.gelf.appender.MessageFormatters;
+
+namespace tests.Formatters
+{
+    public class ExceptionMessageFormatterTests
+    {
+        private ExceptionMessageFormatter formatter;
+
+        [SetUp]
+        public void Setup()
+        {
+            formatter = new ExceptionMessageFormatter();
+        }
+
+        [Test]
+        public void Can_Format_An_Exception()
+        {
+            Assert.That(formatter.CanApply(new Exception()), Is.True);
+            Assert.That(formatter.CanApply(new object()), Is.False);
+        }
+
+        [Test]
+        public void Uses_Exception_Message_For_GelfMessage_ShortMessage()
+        {
+            var gelfMessage = GelfMessage.EmptyGelfMessage;
+
+            formatter.Format(gelfMessage, new Exception("Something bad happend"));
+
+            Assert.That(gelfMessage.ShortMessage, Is.EqualTo("Something bad happend"));
+        }
+
+        [Test]
+        public void Uses_Exception_ToString_For_GelfMessage_FullMessage()
+        {
+            var gelfMessage = GelfMessage.EmptyGelfMessage;
+
+            formatter.Format(gelfMessage, new Exception("Something bad happend"));
+
+            Assert.That(gelfMessage.FullMessage, Is.EqualTo("System.Exception: Something bad happend"));
+        }
+
+        [Test]
+        public void Adds_Exception_Type_Into_Additional_Attribute()
+        {
+            var gelfMessage = GelfMessage.EmptyGelfMessage;
+
+            formatter.Format(gelfMessage, new InvalidOperationException("Something bad happend"));
+
+            Assert.That(gelfMessage["_ExceptionType"], Is.EqualTo("System.InvalidOperationException"));
+        }
+
+        [Test]
+        public void Adds_Stacktrace_Into_Additional_Attribute()
+        {
+            var gelfMessage = GelfMessage.EmptyGelfMessage;
+
+            try
+            {
+                throw new Exception("Bananas");
+            }
+            catch (Exception ex)
+            {
+                formatter.Format(gelfMessage, ex);
+            }
+
+            Assert.That(gelfMessage["_ExceptionStackTrace"], Is.Not.Empty);
+        }
+
+        [Test]
+        public void Adds_InnerExceptionInformation()
+        {
+            var gelfMessage = GelfMessage.EmptyGelfMessage;
+
+            try
+            {
+                throw new InvalidOperationException("House of Commons", new Exception("MP"));
+            }
+            catch (Exception ex)
+            {
+                formatter.Format(gelfMessage, ex);
+            }
+
+            Assert.That(gelfMessage["_ExceptionType"], Contains.Substring("InvalidOperationException"));
+            Assert.That(gelfMessage["_InnerExceptionType"], Is.EqualTo("System.Exception"));
+            Assert.That(gelfMessage["_InnerExceptionMessage"], Is.EqualTo("MP"));
+        }
+
+        [Test]
+        public void Does_Not_Override_Short_or_Full_Message_If_Already_Exist()
+        {
+            var gelfMessage = GelfMessage.EmptyGelfMessage;
+            gelfMessage.ShortMessage = "I'm telling you";
+            gelfMessage.FullMessage = "I'm telling you a long story";
+
+            formatter.Format(gelfMessage, new Exception("Something bad happend"));
+
+            Assert.That(gelfMessage.ShortMessage, Is.EqualTo("I'm telling you")); 
+            Assert.That(gelfMessage.FullMessage, Is.EqualTo("I'm telling you a long story")); 
+        }
+
+        [Test]
+        public void When_Short_or_Full_Message_Already_Exist_Exception_Details_Added_As_Extras()
+        {
+            var gelfMessage = GelfMessage.EmptyGelfMessage;
+            gelfMessage.ShortMessage = "I'm telling you";
+            gelfMessage.FullMessage = "I'm telling you a long story";
+
+            formatter.Format(gelfMessage, new Exception("Something bad happend"));
+
+            Assert.That(gelfMessage["_ExceptionMessage"], Is.EqualTo("Something bad happend"));
+            Assert.That(gelfMessage["_Exception"], Is.EqualTo("System.Exception: Something bad happend"));
+        }
+    }
+}
